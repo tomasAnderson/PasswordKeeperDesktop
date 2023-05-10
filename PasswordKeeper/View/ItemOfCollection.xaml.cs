66397d2dca9b061d120bdb8cc4ec230f1fce5ad7
mr. Anderson <thomasandreson15514@gmail.com>
2023-05-11 00:02:41 +0300

    first design features of project, created viewModel and view

---
+++ PasswordKeeper/View/ItemOfCollection.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace PasswordKeeper.View;
+
+public partial class ItemOfCollection : UserControl
+{
+    public ItemOfCollection()
+    {
+        InitializeComponent();
+    }
+}
